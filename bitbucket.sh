aecbfc81bd4ca332cbe2aaf9714a6e6a3934624c
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-24 16:19:16 +0530

    Update bitbucket.sh

--- bitbucket.sh
+++ bitbucket.sh
@@ -1,1 +1,4 @@
-
+sudo yum install wget git -y
+sudo wget https://www.atlassian.com/software/stash/downloads/binary/atlassian-bitbucket-4.13.0-x64.bin
+sudo chmod +x atlassian-bitbucket-4.13.0-x64.bin
+sudo printf o\n1\n1\n/opt/atlassian/bitbucket/4.13.0\n/var/atlassian/application-data/bitbucket\n7990\n8006\ny\ni\ny\ny\n | ./atlassian-bitbucket-4.13.0-x64.bin
